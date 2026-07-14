a94d2799f92a2b11d285cd3c2979198f58b70f2f
Matt Anis <webdev.omis@gmail.com>
2026-07-14 04:23:33 +0100

    6. Fix the code linting errors
    (had to add prettierrc since zed ide was fighting eslint formatting
    rules)

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ module.exports = [
   },
   js.configs.recommended,
   {
-    files: ['app.js'],
+    files: ['app.js', 'jest.setup.js'],
     languageOptions: {
       ecmaVersion: 2018,
       sourceType: 'commonjs',
@@ -24,15 +24,15 @@ module.exports = [
       }
     },
     rules: {
-      'indent': ['error', 2],
+      indent: ['error', 2],
       'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'never'],
-      'eqeqeq': 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { 'before': true, 'after': true }],
-      'no-console': 0
+      'arrow-spacing': ['error', { before: true, after: true }]
+      // 'no-console': 0
     }
   },
   {
@@ -62,15 +62,15 @@ module.exports = [
     },
     rules: {
       ...react.configs.recommended.rules,
-      'indent': ['error', 2],
+      indent: ['error', 2],
       'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'never'],
-      'eqeqeq': 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { 'before': true, 'after': true }],
-      'no-console': 'error',
+      'arrow-spacing': ['error', { before: true, after: true }],
+      // 'no-console': 'error',
       'react/prop-types': 0
     }
   }
